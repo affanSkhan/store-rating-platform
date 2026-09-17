5c0683cbe04f7ea600528242b7d840c27229cf63
Affan Khan <khanaffan070@gmail.com>
2026-09-17 18:20:12 +0530

    feat: add backend persistence layer

---
+++ server/src/common/common.module.ts
@@ -0,0 +1,16 @@
+import { Global, Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
+import { PrismaService } from './prisma/prisma.service';
+
+@Global()
+@Module({
+  providers: [
+    PrismaService,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    { provide: APP_GUARD, useClass: RolesGuard },
+  ],
+  exports: [PrismaService],
+})
+export class CommonModule {}
